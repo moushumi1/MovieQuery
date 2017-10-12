b49f351de14e0f932d23b5297da0aaf646fa6e06
moushumi1 <32077300+moushumi1@users.noreply.github.com>
2017-10-12 09:51:18 -0700

    Update movieQuery.js

--- movieQuery.js
+++ movieQuery.js
@@ -4,7 +4,8 @@ var movieList = [
   { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "6.7", genre: "horror" },
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
-  { title: "The Matrix Reloaded ", year: "2003", length: 138, rating: "7.2", genre: "sci-fi" }
+  { title: "The Matrix Reloaded ", year: "2003", length: 138, rating: "7.2", genre: "sci-fi" },
+  { title: "Spider-Man: Homecoming", year: "2017", length: 170, rating: "8.2", genre: "action" }
 ];
 
 function getMovieTitle( index ){
@@ -48,14 +49,16 @@ console.log(movieList);*/
 
 function findByTitle( title){
   var matches=[];
+  var temp1=title.toUpperCase();
+  var temp2;
   for(var i= 0; i < movieList.length; i++){
-    if(movieList[i].title.search(title)!= -1){
+    temp2=movieList[i].title.toUpperCase();
+    if(temp2.search(temp1)!= -1){
       matches.push(movieList[i]);
     }
   }
 
   return matches;
 }
-var name = prompt('Input a keyword for search(The first letter should be capital eg. The)');
+var name = prompt('Input a keyword for search');
 console.log(findByTitle(name));
-
